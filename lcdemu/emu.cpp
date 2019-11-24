4dd505b097b15310b26604f4a757ea53b15c94d1
Alexander Vassilev <av@mega.co.nz>
2019-11-25 00:48:26 +0200

    Fixes

--- lcdemu/emu.cpp
+++ lcdemu/emu.cpp
@@ -7,16 +7,25 @@
 class MyApp: public ButtonApp<MyApp, 128, 64, 63, 63>
 {
 public:
+    typedef nsmenu::MenuSystem<decltype(*lcd)> Menu;
+    Menu* menu;
     void onStart()
     {
-        startTimer(10);
-        nsmenu::MenuSystem<decltype(*lcd)> menu(*lcd, "Test menu");
-        menu.addValue<nsmenu::NumValue<uint8_t, 1, &handler>>("test int", 1);
-        const char* vals[] = {"one", "two", "three"};
-        menu.addValue<nsmenu::EnumValue<2, &handler>>("test enum", 1, vals);
-        menu.submenu("SubMenu");
-        menu.render();
+        lcd->setFont(Font_5x7);
+        menu = new Menu(*lcd, "Test menu");
+        menu->addValue<nsmenu::NumValue<uint8_t, 1, &handler>>("test int", 1);
+        menu->addEnum<2, &handler>("test enum", 1, {"one", "two", "three"});
+        menu->submenu("SubMenu");
+        menu->addValue<nsmenu::NumValue<uint8_t, 1, &handler>>("test int", 4);
+        menu->addValue<nsmenu::NumValue<uint8_t, 1, &handler>>("test int", 6);
+        menu->addValue<nsmenu::NumValue<int16_t, 1>>("test int", -800);
+//        menu->addValue<nsmenu::NumValue<float, 1, &handler>>("test float", 1234.567);
+        menu->addEnum<2, &handler>("test bool(enum)", 1, {"yes", "no"});
+
+        menu->addValue<nsmenu::BoolValue<1, &handler>>("test bool", 10);
+        menu->render();
         lcd->updateScreen();
+        startTimer(10);
     }
     static bool handler(uint8_t newVal)
     {
@@ -25,17 +34,14 @@ public:
     }
     static void buttonHandler(uint8_t btn, uint8_t event, void* userp)
     {
-        auto self = static_cast<Self*>(userp);
+        auto self = static_cast<MyApp*>(userp);
         static int ctr = 0;
         printf("%.6d: handler: btn = %u, event = %u\n", ctr++, btn, event);
-
-        if (btn == 1 && event == btn::kEventDown) {
-            for (int i = 0; i< 55; i++) {
-                self->lcd->putsCentered(i, "Test message");
-                self->lcd->updateScreen();
-                wxMilliSleep(10);
-                self->lcd->clear();
-                wxMilliSleep(10);
+        if (event == btn::kEventDown || event ==  btn::kEventRepeat) {
+            if (btn == 1) {
+                self->menu->onButtonUp();
+            } else if (btn == 2) {
+                self->menu->onButtonDown();
             }
         }
     }
